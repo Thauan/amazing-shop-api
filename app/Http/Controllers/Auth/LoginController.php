52a391e24e496f3252a74d4ec1b8d425ca7fdaed
thauan.almeida <thauan.almeida@soterotech.com.br>
2019-12-10 01:50:23 -0300

    Create Service Get Token

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use App\Services\TokenService;
 
 class LoginController extends Controller
 {

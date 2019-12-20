d8bd91f9e236d3eaa314d2c2e32e52f024698772
Thauan <thaubr28@gmail.com>
2019-12-20 02:32:04 -0300

    Update models

--- app/Models/Variant.php
+++ app/Models/Variant.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuids;
 
 class Variant extends Model
 {
-    //
+    use Uuids;
+
+    protected $fillable = [
+        'type', 'value', 'quantity'
+    ];
+
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = false;
+
+    protected $guard_name = 'api';
+
+    protected $dates = ['deleted_at'];
+
+    protected $keyType = 'string';
 }
